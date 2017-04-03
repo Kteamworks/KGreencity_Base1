918cc32d6142a1ddc237820b5ce2ad475b73e05e
sada059 <sada059@gmail.com>
2017-04-03 14:09:10 +0530

    updated code

--- greencity/interface/main/left_nav.php
+++ greencity/interface/main/left_nav.php
@@ -1305,7 +1305,7 @@ if ($GLOBALS['athletic_team']) {
 	  <?php } ?>
 	  <?php genMiscLink('RTop','fin','0',xl('My Patients'),'main/finder/p_dynamic_finder.php'); ?>
 	  <?php if($newcrop_user_role['newcrop_user_role']!='erxdoctor' ||$_SESSION['authUser']=='DR. NIRMALA.B.M') {?>
-	  <?php genMiscLink('RTop','fin','0',xl('InPatients'),'main/finder/p_dynamic_finder_ip.php'); ?>
+	  <?php genMiscLink('RTop','fin','0',xl('In Patients'),'main/finder/p_dynamic_finder_ip.php'); ?>
 	  <?php genMiscLink('RTop','fin','0',xl('Out Patients'),'main/finder/p_dynamic_finder_op.php'); ?>
 	  <?php genMiscLink('RTop','fin','0',xl('Todays Patients'),'main/finder/p_dynamic_finder_tdap.php'); ?>
 	   <?php }?>
