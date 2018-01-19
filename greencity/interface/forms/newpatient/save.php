96352e3c0881ef9492c0780bca038c085d15489b
PavithraSrinivas <pavithras@kavaii.com>
2018-01-19 16:38:30 +0530

    add user while creating the visit

--- greencity/interface/forms/newpatient/save.php
+++ greencity/interface/forms/newpatient/save.php
@@ -106,6 +106,7 @@ if ($mode == 'new')
       "date = '" . add_escape_custom($date) . "', " .
       "onset_date = '" . add_escape_custom($onset_date) . "', " .
       "reason = '" . add_escape_custom($reason) . "', " .
+	  "user = '" . add_escape_custom($_SESSION['authUser']) . "', " .
       "facility = '" . add_escape_custom($facility) . "', " .
       "pc_catid = '" . add_escape_custom($pc_catid) . "', " .
 	   "type = '" . add_escape_custom($type) . "', " .
